61a288c9b0cf940fa92a5ed4dfdaeed9fd02dd0d
Allison He <allisonhe16@gmail.com>
2019-07-08 13:40:53 -0700

    second commit

---
+++ Contents.swift
@@ -0,0 +1,22 @@
+//import UIKit
+////This is a comment, please use comments to help you keep track of your code!
+//
+//// Intention : I would like to create a piece of code that would take each scholars name and output each name into a string
+////Data Types involved : Strings
+//var name = "Sabyatha"
+//var name2 = "Halle"
+//
+////print ("Hi \(name) "+"and"+" \(name2)! We are so happy you're here at KWK Portland!")
+//
+
+//Intention: Working with integers and doubles
+//Data Types: Ints and Doubles
+
+var a = 12.4
+var b = 65
+var c = 31
+var d = 98
+
+
+print(((a+b+c)+Double(d))/4)
+
